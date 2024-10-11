7114e2928c5d119c5d8600257a999c31f9243cfb
Haw Haw Tan <113495100+HawHawTan@users.noreply.github.com>
2024-10-10 20:59:52 -0700

    added some styling in the single page, Got the acf api and able to get the content

--- src/templates/Post.jsx
+++ src/templates/Post.jsx
@@ -9,6 +9,7 @@ const Post = () => {
     const restPath = restBase + `posts?slug=${slug}&_embed`
     const [restData, setData] = useState([])
     const [isLoaded, setLoadStatus] = useState(false)
+    const [acfData, setAcfData] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -17,23 +18,41 @@ const Post = () => {
                 const data = await response.json()
                 setData(data[0])
                 setLoadStatus(true)
+                setAcfData(data[0].acf);
             } else {
                 setLoadStatus(false)
             }
         }
         fetchData()
     }, [restPath])
-        
+    console.log(acfData);
+    
     return (
         <>
         { isLoaded ?
             <>
-                <article id={`post-${restData.id}`}>
+                <article id={`post-${restData.id}`} className="single-page">
                     <h1>{restData.title.rendered}</h1>
                     {restData.featured_media !== 0 && restData._embedded &&
                         <FeaturedImage featuredImageObject={restData._embedded['wp:featuredmedia'][0]} />
                     }
                     <div className="entry-content" dangerouslySetInnerHTML={{__html:restData.content.rendered}}></div>
+                    {acfData && (
+                        <div>
+                            <h3>Key Features:</h3>
+                            <ul>
+                                {acfData.key_features.map((feature,index) =>
+                                    <li key={index}> 
+                                        <strong>{feature.title}: </strong>
+                                        <br />{feature.description}
+                                        <div className='blue-border'></div>
+                                    </li>
+
+                                )}
+                            </ul>
+                           
+                        </div>
+                    )}
                 </article>
             </>
         : 
